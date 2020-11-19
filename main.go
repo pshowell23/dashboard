f99f328f146e1383fe4abf4dce6ea7a007fa5687
Preston Howell <pshowell23@gmail.com>
2020-11-19 14:15:20 -0500

    Get user input

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"./circ"
 )
 
 func main() {
-	circ.CirculationStats(2019, "March")
+	fmt.Println("What year do you want data?")
+	var year int
+	var month string
+	fmt.Scanf("%d", &year)
+	fmt.Println("Which month?")
+	fmt.Scanf("%s", &month)
+	circ.CirculationStats(year, strings.Title(month))
 }
