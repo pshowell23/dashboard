23c9438b836890b07c5125bcb2f0a991d7e2ac94
Preston Howell <pshowell23@gmail.com>
2020-12-14 13:57:03 -0500

    Start webserver

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
-	"./circ"
+	"./webserver"
 )
 
 func main() {
-	fmt.Println("What year do you want data?")
-	var year int
-	var month string
-	fmt.Scanf("%d", &year)
-	fmt.Println("Which month?")
-	fmt.Scanf("%s", &month)
-	circ.CirculationStats(year, strings.Title(month))
+	webserver.StartSite()
 }
